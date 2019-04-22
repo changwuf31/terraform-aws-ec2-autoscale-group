597bb3b62badb7074e939c7e962b9ed8025f71b6
airpaz <airpaz@thor>
2019-04-22 13:27:07 +0700

    use simple tags

--- main.tf
+++ main.tf
@@ -60,15 +60,15 @@ resource "aws_launch_template" "default" {
   }
 }
 
-data "null_data_source" "tags_as_list_of_maps" {
-  count = "${var.enabled == "true" ? length(keys(var.tags)) : 0}"
-
-  inputs = "${map(
-    "key", "${element(keys(var.tags), count.index)}",
-    "value", "${element(values(var.tags), count.index)}",
-    "propagate_at_launch", true
-  )}"
-}
+#data "null_data_source" "tags_as_list_of_maps" {
+#  count = "${var.enabled == "true" ? length(keys(var.tags)) : 0}"
+#
+#  inputs = "${map(
+#    "key", "${element(keys(var.tags), count.index)}",
+#    "value", "${element(values(var.tags), count.index)}",
+#    "propagate_at_launch", true
+#  )}"
+#}
 
 resource "aws_autoscaling_group" "default" {
   count = "${var.enabled == "true" ? 1 : 0}"
@@ -99,7 +99,8 @@ resource "aws_autoscaling_group" "default" {
     version = "${aws_launch_template.default.latest_version}"
   }
 
-  tags = ["${data.null_data_source.tags_as_list_of_maps.*.outputs}"]
+  tags = "${module.label.tags}"
+#  tags = ["${data.null_data_source.tags_as_list_of_maps.*.outputs}"]
 
   lifecycle {
     create_before_destroy = true
